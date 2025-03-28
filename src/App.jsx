c1ccda04fd4bb0de9a872216538223ffab095a8d
Jako <43934830+jfrb95@users.noreply.github.com>
2025-03-28 19:10:47 +0000

    add pokemon fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.css'
 
 function App() {
   
+  const rootUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const [nameList, setNameList] = useState([]);
+  const [imgList, setImgList] = useState([]);
+
+  const pokedexNumbers = [1, 6, 8, 11, 15, 17, 19, 25, 28, 34, 39, 41];
+  
+  
+  {
+  const request = new Request(rootUrl + 1);
+
+  useEffect(() => {
+    (async function fetchPokemon() {
+      await fetch(request)
+        .then((response) => response.json())
+        .then((response) => console.log(response.name));
+    })()
+  })
+  }
+
+  function Card() {
+
+  }
+
   return (
     <>
       <header>
@@ -10,7 +34,7 @@ function App() {
         <p>Click the images to gain points, but don't click the same one twice!</p>
       </header>
       <main>
-
+        <Card/>
       </main>
     </>
   )
