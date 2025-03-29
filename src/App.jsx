7e7c9636f75712c64c8b3428bacf0a8d3a17890f
Jako <43934830+jfrb95@users.noreply.github.com>
2025-03-29 15:13:08 +0000

    begin useEffect on array of pokedex numbers

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,38 @@ function App() {
   
   const rootUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-  const [nameList, setNameList] = useState([]);
-  const [imgList, setImgList] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const pokedexNumbers = [1, 6, 8, 11, 15, 17, 19, 25, 28, 34, 39, 41];
-  
-  
-  {
-  const number = 31
-  const request = new Request(rootUrl + number);
+
+  //loggin pokemonList after running useEffect gives an array of 12 undefineds
 
   useEffect(() => {
     (async function fetchPokemon() {
-      await fetch(request)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`Failed to fetch Pokemon #${number}`);
-          }
-          return response.json()
-        })
+      const pokePromises = pokedexNumbers.map(number => {
+        const request = new Request(rootUrl + number);
+        fetch(request)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`Failed to fetch Pokemon #${number}`);
+            }
+            return response.json()
+          });
+      })
+
+      const pokeResults = await Promise.all(pokePromises);
+      const pokemon = pokeResults.reduce((acc, curr) => {
+        acc.push(curr)
+        return acc
+      }, [])
+      setPokemonList(pokemon);
     })();
-  })
+    console.log(pokemonList);
+  }, []);
+  
+  function logPokemonList() {
+    console.log(pokemonList);
   }
 
   function Card() {
@@ -41,6 +51,7 @@ function App() {
         <p>Click the images to gain points, but don't click the same one twice!</p>
       </header>
       <main>
+        <button onClick={logPokemonList}>Log Pokemon List</button>
         <Card/>
       </main>
     </>
